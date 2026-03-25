1c274497c068f4ed615c7c4fcc5a9b99503a96bc
C BHANU PRAKASH <bhanu18042005@gmail.com>
2026-03-25 15:29:16 +0530

    added direct link even if .env crashes

--- src/App.js
+++ src/App.js
@@ -7,8 +7,7 @@ function App() {
   const [editingId, setEditingId] = useState(null);
   const [editText, setEditText] = useState("");
 
-const API = "https://dummyurl.com/api";
-  useEffect(() => {
+const API = process.env.REACT_APP_API_URL || "http://localhost:8080/api/todos";  useEffect(() => {
     fetchTodos();
   }, []);
 
